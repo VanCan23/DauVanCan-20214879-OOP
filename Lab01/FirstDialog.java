3c2401c5ad45d9d1afd4991658b669b7eb806c61
VanCan23 <130449825+VanCan23@users.noreply.github.com>
2023-04-14 14:40:43 +0700

    Add files via upload

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,8 @@
+//Example 2:first diaglog.java
+import javax.swing.JOptionPane;
+public class FirstDialog{
+    public static void main(String[]args){
+        JOption.showMessageDialog(null,"Hello world! How are you?");
+        system.exit(0);
+    }
+}
