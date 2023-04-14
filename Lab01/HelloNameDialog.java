8b853174cbbf4b4e5d5e12594e51bcffe6b29712
VanCan23 <130449825+VanCan23@users.noreply.github.com>
2023-04-14 14:44:48 +0700

    Add files via upload

---
+++ Lab01/HelloNameDialog.java
@@ -0,0 +1,10 @@
+//Example 2:first diaglog.java
+import javax.swing.JOptionPane;
+public class HelloNameDialog{
+    public static void main(String[]args){
+        String result;
+        resul=JOptionPane.showInputDialog("Please enter your name:");
+        JOption.showMessageDialog(null,"Hi"+result+"!");
+        system.exit(0);
+    }
+}
