d917075f08230e1c75ebbde6310295b91ea60df2
VanCan23 <130449825+VanCan23@users.noreply.github.com>
2023-04-14 14:34:26 +0700

    Update HelloWorld.java

--- Lab01/HelloWorld.java
+++ Lab01/HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("xin chao \n cac ban");
         System.out.println("Hello \t World!"); 
-    }
+    } //end of method main
 }
